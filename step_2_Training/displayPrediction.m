215b120e2e78d2261773fa323e6f2ef0b5f5e6b6
kenanGonnot <78441094+kenanGonnot@users.noreply.github.com>
2021-02-17 20:25:19 +0100

    update displayPred

--- step_2_Training/displayPrediction.m
+++ step_2_Training/displayPrediction.m
@@ -7,7 +7,8 @@ for i = 1:size(Xtest, 1)
     displayData(Xtest(rp(i), :),12);
 
     pred = predict(Theta1, Theta2, Xtest(rp(i),:));
-    fprintf('\nNeural Network Prediction: %d (digit %d)\n', pred, mod(pred, 10));
+    %fprintf('\nNeural Network Prediction: %d (digit %d)\n', pred, mod(pred, 10));
+    fprintf('\nNeural Network Prediction: %d (true : %d)\n', pred, Ytest(rp(i)));
     
     % Pause with quit option
     s = input('Paused - press enter to continue, q to exit:','s');
